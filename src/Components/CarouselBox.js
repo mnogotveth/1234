6a67e5ac167eb67b5b1b5a3813128d56b691c60c
mnogotveth <lalkapetrovich@gmail.com>
2020-12-14 16:26:17 +0700

    slider

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import forestImg from '../Assets/bg1.jpg';
+import bforestImg from '../Assets/bg2.jpg';
+import aforestImg from '../Assets/bg3.jpg';
+
+export default class CarouselBox extends Component {
+    render() {
+        return (
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={forestImg}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bforestImg}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={aforestImg}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CarouselBox from '../Components/CarouselBox';
 
 export default class Home extends Component {
     render() {
         return (
-            <div> 
-                <h1>HELLO</h1>
+            <div>
+            <CarouselBox />
             </div>
         )
     }
