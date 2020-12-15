1a53f1aad6d8cd88ce4617f00bf48f35e8c74e7e
mnogotveth <lalkapetrovich@gmail.com>
2020-12-15 13:55:18 +0700

    mini test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './Components/Header';
+import ReactTestUtils from 'react-dom/test-utils'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+
+it('has an h2 tag', () => {
+ 
+  const component = ReactTestUtils.renderIntoDocument(<Header/>);    
+  var h2 = ReactTestUtils.findRenderedDOMComponentWithTag(
+   component, 'h2'
+ );
+ 
 });
+
+
+
+
